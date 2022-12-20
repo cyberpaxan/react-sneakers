99ccdf9f13e9dce15b22f2c083ec1761bd97b472
cyberpaxan <igorl098765@gmail.com>
2022-12-20 16:00:12 +0500

    first part of site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='wrapper clear'>
+            <header className='d-flex justify-between align-center p-40'>
+                <div className='d-flex align-center'>
+                    <img width={40} height={40} src='/img/logo.png' alt='pic' />
+                    <div className='headerInfo'>
+                        <h3 className='text-uppercase'>React Sneakers</h3>
+                        <p className='opacity-5'>Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+
+                <ul className='d-flex'>
+                    <li className='mr-30'>
+                        <img
+                            width={18}
+                            height={18}
+                            src='/img/cart.svg'
+                            alt='pic'
+                        />
+                        <span>1205 руб.</span>
+                    </li>
+                    <li>
+                        <img
+                            width={18}
+                            height={18}
+                            src='/img/user.svg'
+                            alt='pic'
+                        />
+                    </li>
+                </ul>
+            </header>
+            <div className='content p-40'>
+                <h1 className='mb-40'>Все кроссовки</h1>
+                <div className='sneakers d-flex'>
+                    <div className='card'>
+                        <img
+                            width={133}
+                            height={112}
+                            src='/img/sneakers/1.jpg'
+                            alt='pic'
+                        />
+                        <h5>Мужские кроссовки Nike Blazer Mid </h5>
+                        <div className='d-flex justify-between align-center'>
+                            <div className='d-flex flex-column'>
+                                <span>Цена:</span>
+                                <b>12 990 руб.</b>
+                            </div>
+
+                            <button className='button'>
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src='/img/plus.svg'
+                                    alt='pic'
+                                />
+                            </button>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <img
+                            width={133}
+                            height={112}
+                            src='/img/sneakers/2.jpg'
+                            alt='pic'
+                        />
+                        <h5>Мужские кроссовки Nike Blazer Mid </h5>
+                        <div className='d-flex justify-between align-center'>
+                            <div className='d-flex flex-column'>
+                                <span>Цена:</span>
+                                <b>12 990 руб.</b>
+                            </div>
+
+                            <button className='button'>
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src='/img/plus.svg'
+                                    alt='pic'
+                                />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
