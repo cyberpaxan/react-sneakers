4c724825718c98872e49a2841f801a193b05c567
cyberpaxan <igorl098765@gmail.com>
2022-12-22 10:36:54 +0500

    finished layout + make cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,81 @@
 function App() {
     return (
         <div className='wrapper clear'>
+            <div className='overlay'>
+                <div className='drawer'>
+                    <h2 className='d-flex justify-between '>
+                        Корзина
+                        <img
+                            className='removeBtn cu-p'
+                            src='/img/btn-remove.svg'
+                            alt='Remove'
+                        />
+                    </h2>
+
+                    <div className='items'>
+                        <div className='cartItem d-flex align-center'>
+                            <img
+                                className='mr-15'
+                                width={80}
+                                height={80}
+                                src='/img/sneakers/1.jpg'
+                                alt='Sneakers'
+                            />
+                            <div className='mr-20'>
+                                <p className='mb-5'>
+                                    Мужские кроссовки Nike Blazer Mid
+                                </p>
+                                <b>12 990 руб.</b>
+                            </div>
+                            <img
+                                className='removeBtn'
+                                src='/img/btn-remove.svg'
+                                alt='Remove'
+                            />
+                        </div>
+
+                        <div className='cartItem d-flex align-center'>
+                            <img
+                                className='mr-15'
+                                width={80}
+                                height={80}
+                                src='/img/sneakers/1.jpg'
+                                alt='Sneakers'
+                            />
+                            <div className='mr-20'>
+                                <p className='mb-5'>
+                                    Мужские кроссовки Nike Blazer Mid
+                                </p>
+                                <b>12 990 руб.</b>
+                            </div>
+                            <img
+                                className='removeBtn'
+                                src='/img/btn-remove.svg'
+                                alt='Remove'
+                            />
+                        </div>
+                    </div>
+                    <div className='cartTotalBlock'>
+                        <ul>
+                            <li>
+                                <span>Итого:</span>
+                                <div></div>
+                                <b>21 498 руб.</b>
+                            </li>
+                            <li className='d-flex'>
+                                <span>Налог 5%:</span>
+                                <div></div>
+                                <b>1074 руб.</b>
+                            </li>
+                        </ul>
+                        <button className='greenButton'>
+                            Оформить заказ
+                            <img src='/img/arrow.svg' alt='Arrow' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
             <header className='d-flex justify-between align-center p-40'>
                 <div className='d-flex align-center'>
                     <img width={40} height={40} src='/img/logo.png' alt='pic' />
@@ -31,16 +106,25 @@ function App() {
                 </ul>
             </header>
             <div className='content p-40'>
-                <h1 className='mb-40'>Все кроссовки</h1>
+                <div className=' d-flex align-center justify-between mb-40'>
+                    <h1>Все кроссовки</h1>
+                    <div className='search-block d-flex'>
+                        <img src='/img/search.svg' alt='Search' />
+                        <input placeholder='Поиск...' />
+                    </div>
+                </div>
                 <div className='sneakers d-flex'>
                     <div className='card'>
+                        <div className='favorite'>
+                            <img src='/img/unliked.svg' alt='Unliked' />
+                        </div>
                         <img
                             width={133}
                             height={112}
                             src='/img/sneakers/1.jpg'
                             alt='pic'
                         />
-                        <h5>Мужские кроссовки Nike Blazer Mid </h5>
+                        <h5>Мужские кроссовки Nike Blazer Mid</h5>
                         <div className='d-flex justify-between align-center'>
                             <div className='d-flex flex-column'>
                                 <span>Цена:</span>
